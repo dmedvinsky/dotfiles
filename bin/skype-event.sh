589280d74c08bc1f82f72c623c4124f478dfb200
Dmitry Medvinsky <dmedvinsky@gmail.com>
2012-06-26 13:18:24 +0400

    Skype updated, quick hack for notification script

--- bin/skype-event.sh
+++ bin/skype-event.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh
-SKYPE_ID="$1"
-EVENT_TYPE="$2"
+#!/usr/bin/env bash
+EVENT_TYPE="$1"
+SKYPE_ID="$2"
 SKYPE_NAME="$3"
 
 which xdotool   >/dev/null 2>&1 || exit 1
@@ -12,25 +12,29 @@ urgentify() {
         seturgent "$id"
     done
 }
-
 findChat() {
     local ids=""
-    ids=$(xdotool search --all --limit 1 --name "${SKYPE_NAME}.*Skype.*Chat")
-    if [ -z "$ids" ]; then
-        ids=$(xdotool search --name ".* - Skype.*(Beta)")
-    fi
+    ids=$(xdotool search --class "Skype")
     echo $ids
 }
+# findChat() {
+#     local ids=""
+#     ids=$(xdotool search --all --limit 1 --name "${SKYPE_NAME}.*Skype.*Chat")
+#     if [ -z "$ids" ]; then
+#         ids=$(xdotool search --name ".* - Skype.*(Beta)")
+#     fi
+#     echo $ids
+# }
 
 
 case "$EVENT_TYPE" in
     ChatIncoming)
-        urgentify `findChat`
+        urgentify $(findChat)
         ;;
     ChatIncomingInitial)
-        urgentify `findChat`
+        urgentify $(findChat)
         ;;
     *)
-        # echo "Type $EVENT_TYPE    Id $SKYPE_ID    Name $SKYPE_NAME" >> /tmp/skype.log
+        echo "Type $EVENT_TYPE    Id $SKYPE_ID    Name $SKYPE_NAME" >> /tmp/skype.log
         ;;
 esac
