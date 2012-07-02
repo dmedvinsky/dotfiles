2f56c1ae34c7a03262d67c82e6b092aa753ab642
Dmitry Medvinsky <dmedvinsky@gmail.com>
2012-07-02 12:06:53 +0400

    Fix PATH

--- config.fish
+++ config.fish
@@ -5,11 +5,13 @@ set PATH "/bin/"
 set PATH "/sbin/" $PATH
 set PATH "/usr/bin/" $PATH
 set PATH "/usr/sbin/" $PATH
-set PATH "/usr/local/bin" $PATH
-set PATH "/usr/local/sbin" $PATH
+test -d "/usr/local/bin"; and set PATH "/usr/local/bin" $PATH
+test -d "/usr/local/sbin"; and set PATH "/usr/local/sbin" $PATH
+if test -d "$HOME/.android/sdk"
+    set PATH "$HOME/.android/sdk/tools" $PATH
+    set PATH "$HOME/.android/sdk/platform-tools" $PATH
+end
 # set PATH "$HOME/.cabal/bin" $PATH
-# set PATH "$HOME/.android/sdk/tools" $PATH
-# set PATH "$HOME/.android/sdk/platform-tools" $PATH
 set PATH "$HOME/bin" $PATH
 
 set -g -x fish_greeting ''
