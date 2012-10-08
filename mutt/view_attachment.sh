464b3f202153e1755f15916d2b1c75939cad5e31
Dmitry Medvinsky <dmedvinsky@gmail.com>
2012-10-08 11:06:31 +0400

    Fix view_attachment

--- mutt/view_attachment.sh
+++ mutt/view_attachment.sh
@@ -3,10 +3,10 @@ tmpdir="/tmp/mutt_attach"
 open_with=$1
 path=$2
 filename=$(basename "${path}")
-newpath="$tmpdir/$filename"
+newpath="${tmpdir}/${filename}"
 
 mkdir -p "${tmpdir}"
 rm -f "${tmpdir}/*"
 cp "${path}" "${newpath}"
 
-"${open_with}" "${newpath}" &
+${open_with} "${newpath}" &
