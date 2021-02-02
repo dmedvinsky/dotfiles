95eac44ec584ad0a4163a7c4ee814b522d4ffc40
Dmitry Medvinsky <me@dmedvinsky.name>
2021-02-02 11:27:28 +0300

    Remove some unused fish stuff

--- fish/functions/ansible-setup.fish
+++
@@ -1,4 +0,0 @@
-function ansible-setup
-    pyenv shell ansible
-    . $HOME/src/universe/ansible/hacking/env-setup.fish
-end
--- fish/functions/start_ssh_agent.fish
+++
@@ -1,7 +0,0 @@
-function start_ssh_agent
-    setenv SSH_ENV $HOME/.ssh/environment
-    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
-    chmod 600 $SSH_ENV
-    . $SSH_ENV > /dev/null
-    ssh-add
-end
